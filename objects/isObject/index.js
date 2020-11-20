c22772fb543c0d6589f3e8f3fa40620a32c5d08a
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-11-20 17:10:13 +0100

    added doc block

--- objects/isObject/index.js
+++ objects/isObject/index.js
@@ -1,2 +1,7 @@
+/**
+ * The function checks if the value passed is an object or not. 
+ * @param {Object} obj The value we need to check if is an object.
+ * @returns {boolean} If is an object or not.
+ */
 export const isObject = 
 	(obj) => obj?.toString() === "[object Object]";
